5be29f568e98255011f1e9323302e2435a39aee0
nigh3252-prog <nigh3252@gmail.com>
2026-07-06 15:33:03 -0400

    Tag stance cards with preferred weapons

--- src/stance-cards.js
+++ src/stance-cards.js
@@ -1,37 +1,217 @@
 export const STANCE_CARDS = [
-  {id:'S01', name:'S01 Hammerfall Guard', chain:['vertical10','vertical5','vertical9']},
-  {id:'S02', name:'S02 Bell Ringer', chain:['horizontal6','vertical10','vertical15']},
-  {id:'S03', name:'S03 Iron Chapel', chain:['vertical5','horizontal4','vertical9']},
-  {id:'S04', name:'S04 Anvil Step', chain:['vertical8','vertical10','horizontal5']},
-  {id:'S05', name:'S05 Grave Mallet', chain:['horizontal3','vertical5','vertical15']},
+  {
+    id:'S01',
+    name:'S01 Hammerfall Guard',
+    chain:['vertical10','vertical5','vertical9'],
+    preferredWeapons:['warhammer','mace'],
+    styleTags:['blunt','vertical','heavy']
+  },
+  {
+    id:'S02',
+    name:'S02 Bell Ringer',
+    chain:['horizontal6','vertical10','vertical15'],
+    preferredWeapons:['mace','warhammer'],
+    styleTags:['blunt','horizontal','vertical']
+  },
+  {
+    id:'S03',
+    name:'S03 Iron Chapel',
+    chain:['vertical5','horizontal4','vertical9'],
+    preferredWeapons:['longsword','warhammer'],
+    styleTags:['vertical','heavy','balanced']
+  },
+  {
+    id:'S04',
+    name:'S04 Anvil Step',
+    chain:['vertical8','vertical10','horizontal5'],
+    preferredWeapons:['warhammer','battleaxe'],
+    styleTags:['heavy','blunt','cleave']
+  },
+  {
+    id:'S05',
+    name:'S05 Grave Mallet',
+    chain:['horizontal3','vertical5','vertical15'],
+    preferredWeapons:['mace','warhammer'],
+    styleTags:['blunt','impact','vertical']
+  },
 
-  {id:'S06', name:'S06 Split Oak', chain:['vertical2','vertical7','vertical9']},
-  {id:'S07', name:'S07 Butcher Measure', chain:['vertical3','horizontal5','vertical15']},
-  {id:'S08', name:'S08 Left-Hand Reaper', chain:['vertical4','horizontal2','vertical9']},
-  {id:'S09', name:'S09 Deep Launch', chain:['vertical8','stab4','vertical9']},
-  {id:'S10', name:'S10 Guillotine Reel', chain:['horizontal','vertical16','vertical15']},
+  {
+    id:'S06',
+    name:'S06 Split Oak',
+    chain:['vertical2','vertical7','vertical9'],
+    preferredWeapons:['battleaxe','greatsword'],
+    styleTags:['vertical','cleave','heavy']
+  },
+  {
+    id:'S07',
+    name:'S07 Butcher Measure',
+    chain:['vertical3','horizontal5','vertical15'],
+    preferredWeapons:['battleaxe','claymore'],
+    styleTags:['slice','cleave','heavy']
+  },
+  {
+    id:'S08',
+    name:'S08 Left-Hand Reaper',
+    chain:['vertical4','horizontal2','vertical9'],
+    preferredWeapons:['battleaxe','claymore'],
+    styleTags:['slice','vertical','cleave']
+  },
+  {
+    id:'S09',
+    name:'S09 Deep Launch',
+    chain:['vertical8','stab4','vertical9'],
+    preferredWeapons:['spear','greatsword'],
+    styleTags:['reach','stab','heavy']
+  },
+  {
+    id:'S10',
+    name:'S10 Guillotine Reel',
+    chain:['horizontal','vertical16','vertical15'],
+    preferredWeapons:['greatsword','claymore'],
+    styleTags:['flashy','greatblade','slice']
+  },
 
-  {id:'S11', name:'S11 Saber Waltz', chain:['horizontal6','horizontal4','horizontal5']},
-  {id:'S12', name:'S12 Crescent Cut', chain:['vertical5','horizontal','horizontal2']},
-  {id:'S13', name:'S13 Red Ribbon', chain:['horizontal4','vertical11','horizontal5']},
-  {id:'S14', name:'S14 Street Duelist', chain:['stab','horizontal6','horizontal2']},
-  {id:'S15', name:'S15 Open Palm Cut', chain:['vertical8','horizontal3','horizontal']},
+  {
+    id:'S11',
+    name:'S11 Saber Waltz',
+    chain:['horizontal6','horizontal4','horizontal5'],
+    preferredWeapons:['saber'],
+    styleTags:['saber','horizontal','slice','duelist']
+  },
+  {
+    id:'S12',
+    name:'S12 Crescent Cut',
+    chain:['vertical5','horizontal','horizontal2'],
+    preferredWeapons:['saber','longsword'],
+    styleTags:['slice','horizontal','fast']
+  },
+  {
+    id:'S13',
+    name:'S13 Red Ribbon',
+    chain:['horizontal4','vertical11','horizontal5'],
+    preferredWeapons:['saber'],
+    styleTags:['saber','horizontal','slice','flashy']
+  },
+  {
+    id:'S14',
+    name:'S14 Street Duelist',
+    chain:['stab','horizontal6','horizontal2'],
+    preferredWeapons:['saber','dagger'],
+    styleTags:['duelist','fast','stab','horizontal']
+  },
+  {
+    id:'S15',
+    name:'S15 Open Palm Cut',
+    chain:['vertical8','horizontal3','horizontal'],
+    preferredWeapons:['saber','longsword'],
+    styleTags:['slice','balanced','horizontal']
+  },
 
-  {id:'S16', name:'S16 Needle Line', chain:['stab3','stab','stab6']},
-  {id:'S17', name:'S17 High Needle', chain:['stab2','stab4','stab6']},
-  {id:'S18', name:'S18 False Invitation', chain:['stab5','horizontal6','stab']},
-  {id:'S19', name:'S19 Surgeon Ladder', chain:['stab3','vertical10','stab6']},
-  {id:'S20', name:'S20 Pike Drill', chain:['stab2','vertical2','stab4']},
+  {
+    id:'S16',
+    name:'S16 Needle Line',
+    chain:['stab3','stab','stab6'],
+    preferredWeapons:['rapier'],
+    styleTags:['pierce','stab','duelist']
+  },
+  {
+    id:'S17',
+    name:'S17 High Needle',
+    chain:['stab2','stab4','stab6'],
+    preferredWeapons:['rapier','spear'],
+    styleTags:['pierce','stab','reach']
+  },
+  {
+    id:'S18',
+    name:'S18 False Invitation',
+    chain:['stab5','horizontal6','stab'],
+    preferredWeapons:['rapier','dagger'],
+    styleTags:['duelist','pierce','fast']
+  },
+  {
+    id:'S19',
+    name:'S19 Surgeon Ladder',
+    chain:['stab3','vertical10','stab6'],
+    preferredWeapons:['rapier','dagger'],
+    styleTags:['pierce','precision','fast']
+  },
+  {
+    id:'S20',
+    name:'S20 Pike Drill',
+    chain:['stab2','vertical2','stab4'],
+    preferredWeapons:['spear'],
+    styleTags:['reach','pierce','stab']
+  },
 
-  {id:'S21', name:'S21 Boar Spear', chain:['vertical8','stab','stab6']},
-  {id:'S22', name:'S22 Hook and Thrust', chain:['horizontal3','stab5','stab4']},
-  {id:'S23', name:'S23 Low Knife', chain:['stab3','horizontal6','vertical5']},
-  {id:'S24', name:'S24 Rat Step', chain:['vertical10','stab5','horizontal6']},
-  {id:'S25', name:'S25 Alley Fang', chain:['horizontal3','stab3','stab']},
+  {
+    id:'S21',
+    name:'S21 Boar Spear',
+    chain:['vertical8','stab','stab6'],
+    preferredWeapons:['spear'],
+    styleTags:['reach','pierce','heavy']
+  },
+  {
+    id:'S22',
+    name:'S22 Hook and Thrust',
+    chain:['horizontal3','stab5','stab4'],
+    preferredWeapons:['spear','saber'],
+    styleTags:['reach','stab','horizontal']
+  },
+  {
+    id:'S23',
+    name:'S23 Low Knife',
+    chain:['stab3','horizontal6','vertical5'],
+    preferredWeapons:['dagger'],
+    styleTags:['fast','knife','pierce']
+  },
+  {
+    id:'S24',
+    name:'S24 Rat Step',
+    chain:['vertical10','stab5','horizontal6'],
+    preferredWeapons:['dagger'],
+    styleTags:['fast','knife','duelist']
+  },
+  {
+    id:'S25',
+    name:'S25 Alley Fang',
+    chain:['horizontal3','stab3','stab'],
+    preferredWeapons:['dagger'],
+    styleTags:['fast','pierce','street']
+  },
 
-  {id:'S26', name:'S26 Long Blade Form', chain:['vertical2','horizontal','vertical15']},
-  {id:'S27', name:'S27 Crown Splitter', chain:['vertical3','vertical4','vertical9']},
-  {id:'S28', name:'S28 Doorbreaker', chain:['horizontal5','vertical7','stab6']},
-  {id:'S29', name:'S29 Crossguard Bloom', chain:['vertical11','horizontal2','stab4']},
-  {id:'S30', name:'S30 Spiral Oath', chain:['vertical16','horizontal4','stab5']}
+  {
+    id:'S26',
+    name:'S26 Long Blade Form',
+    chain:['vertical2','horizontal','vertical15'],
+    preferredWeapons:['longsword','claymore'],
+    styleTags:['balanced','slice','longblade']
+  },
+  {
+    id:'S27',
+    name:'S27 Crown Splitter',
+    chain:['vertical3','vertical4','vertical9'],
+    preferredWeapons:['claymore','greatsword'],
+    styleTags:['vertical','greatblade','heavy']
+  },
+  {
+    id:'S28',
+    name:'S28 Doorbreaker',
+    chain:['horizontal5','vertical7','stab6'],
+    preferredWeapons:['greatsword','battleaxe'],
+    styleTags:['heavy','cleave','finisher']
+  },
+  {
+    id:'S29',
+    name:'S29 Crossguard Bloom',
+    chain:['vertical11','horizontal2','stab4'],
+    preferredWeapons:['longsword','saber'],
+    styleTags:['balanced','duelist','mixed']
+  },
+  {
+    id:'S30',
+    name:'S30 Spiral Oath',
+    chain:['vertical16','horizontal4','stab5'],
+    preferredWeapons:['greatsword','claymore'],
+    styleTags:['flashy','mixed','greatblade']
+  }
 ];
