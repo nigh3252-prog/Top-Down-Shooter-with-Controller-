017026e7679d3e0638dfc8b53e329dfcf54984fb
nigh3252-prog <nigh3252@gmail.com>
2026-07-15 15:49:51 -0400

    Apply courtyard visual skin to maze combat world

--- src/hex-maze-renderer.js
+++ src/hex-maze-renderer.js
@@ -2,10 +2,15 @@ export { drawMazeDebug } from './hex-maze-renderer-base.js';
 
 import { createMazeWorld as createBaseMazeWorld } from './hex-maze-renderer-base.js';
 import { configuredHexSize } from './maze-runtime-settings.js';
+import { applyAkaiMazeStyle, installAkaiInterfaceStyle } from './akai-visual-style.js';
+
+installAkaiInterfaceStyle();
 
 export function createMazeWorld(options = {}){
-  return createBaseMazeWorld({
+  const configuredOptions = {
     ...options,
     hexSize:configuredHexSize(options.hexSize ?? 2.6),
-  });
+  };
+  const world = createBaseMazeWorld(configuredOptions);
+  return applyAkaiMazeStyle({ ...configuredOptions, world });
 }
