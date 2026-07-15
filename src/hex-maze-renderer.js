a4c7f1223e543e87a2aa668be28dabd80f49c63c
nigh3252-prog <nigh3252@gmail.com>
2026-07-15 18:07:01 -0400

    Apply compact HUD after Akai visual skin

--- src/hex-maze-renderer.js
+++ src/hex-maze-renderer.js
@@ -3,8 +3,10 @@ export { drawMazeDebug } from './hex-maze-renderer-base.js';
 import { createMazeWorld as createBaseMazeWorld } from './hex-maze-renderer-base.js';
 import { configuredHexSize } from './maze-runtime-settings.js';
 import { applyAkaiMazeStyle, installAkaiInterfaceStyle } from './akai-visual-style.js';
+import { installAkaiCompactInterface } from './akai-compact-interface.js';
 
 installAkaiInterfaceStyle();
+installAkaiCompactInterface();
 
 export function createMazeWorld(options = {}){
   const configuredOptions = {
